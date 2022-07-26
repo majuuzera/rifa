29585671d42c437625582bdae7f3262c49929c39
majuuzera <majuthelink@gmail.com>
2022-07-26 10:15:28 -0300

    COMMIT

--- src/Vendedor/model/list-vendedor.php
+++ src/Vendedor/model/list-vendedor.php
@@ -11,7 +11,7 @@
     $colunas = $requestData['columns'];
 
     //Preparar o comando sql para obter os dados da categoria
-    $sql = "SELECT ID, NOME FROM TIPO WHERE 1=1 ";
+    $sql = "SELECT ID, NOME, CELULAR FROM VENDEDOR WHERE 1=1 ";
 
     //Obter o total de registros cadastrados
     $resultado = $pdo->query($sql);
@@ -23,7 +23,8 @@
         //Montar a expressão lógica que irá compor os filtros
         //Aqui você deverá determinar quais colunas farão parte do filtro
         $sql .= " AND (ID LIKE '$filtro%' ";
-        $sql .= " OR NOME LIKE '$filtro%') ";
+        $sql .= " OR NOME LIKE '$filtro%' ";
+        $sql .= " OR CELULAR LIKE '$filtro%') ";
     }
     
     //Obter o total dos dados filtrados
