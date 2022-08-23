8fe40945fa4c8b2aedd0f0c0d1f37cfc36965d19
majuuzera <majuthelink@gmail.com>
2022-08-23 10:12:41 -0300

    commitok

--- src/Vendedor/model/validate-vendedor.php
+++ src/Vendedor/model/validate-vendedor.php
@@ -4,13 +4,13 @@ session_start();
 
 if(!isset($_SESSION['NOME']) && !isset($_SESSION['TIPO']) ){
     $dados = array(
-        'tipo' =>  'error';
+        'tipo' =>  'error',                                             
         'mensagem' => 'você não está autenticado'
     );
 }else{
     $dados = array(
-        'tipo' =>  'success';
-        'mensagem' => 'bem-vindo, ' . $_SESSION['NOME'];
+        'tipo' =>  'success',
+        'mensagem' => 'bem-vindo, ' . $_SESSION['NOME'] 
     );
 }
 echo json_encode($dados);
